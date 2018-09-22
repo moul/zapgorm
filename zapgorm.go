0a8e3ecbee39f9099eefa243a1d0db36f5758fa1
Manfred Touron <m@42.am>
2018-09-22 23:13:58 +0200

    feat: add zapgorm

---
+++ zapgorm.go
@@ -0,0 +1,37 @@
+package zapgorm
+
+import (
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type Logger struct {
+	zap *zap.Logger
+}
+
+func New(logger *zap.Logger) Logger {
+	return Logger{zap: logger}
+}
+
+func (l *Logger) Print(values ...interface{}) {
+	if len(values) < 2 {
+		return
+	}
+
+	switch values[0] {
+	case "sql":
+		l.zap.Debug("gorm.debug.sql",
+			zap.String("query", values[3].(string)),
+			zap.Any("values", values[4]),
+			zap.Duration("duration", values[2].(time.Duration)),
+			zap.Int64("affected-rows", values[5].(int64)),
+			zap.String("source", values[1].(string)), // if AddCallerSkip(6) is well defined, we can safely remove this field
+		)
+	default:
+		l.zap.Debug("gorm.debug.other",
+			zap.Any("values", values[2:]),
+			zap.String("source", values[1].(string)), // if AddCallerSkip(6) is well defined, we can safely remove this field
+		)
+	}
+}
